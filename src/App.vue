780c0fb46a755d351a152a871834cc777311725b
Onur Kantarci <onurkntrc@gmail.com>
2024-01-17 13:18:13 +0300

    refactor: Remove unnecessary routes

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,6 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
