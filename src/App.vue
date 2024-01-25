f197748e22e3ec080a95e1a6393c4974886bebb3
Onur Kantarci <onurkntrc@gmail.com>
2024-01-25 21:35:00 +0300

    refactor: Leave no selectable route available

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <div id="app">
+    <div id="nav"><router-link to="/"></router-link></div>
     <router-view />
   </div>
 </template>
-
-<style scoped>
-#app {
-  font-family: Avenir, Muli, sans-serif !important;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-#nav a {
-  font-weight: bold;
-  color: #4f4f4f;
-}
-#nav a.router-link-exact-active {
-  color: #46bbb3;
-}
-</style>
