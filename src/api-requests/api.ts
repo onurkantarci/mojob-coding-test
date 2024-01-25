d6233c9d8c9dbbbeba3734c986ea87ef5189e739
Onur Kantarci <onurkntrc@gmail.com>
2024-01-25 21:40:49 +0300

    feat: Adjust api.ts for fetching

--- src/api-requests/api.ts
+++ src/api-requests/api.ts
@@ -1,5 +1,5 @@
 import type { AxiosStatic } from 'axios'
-import type { IPage, PositionFunction } from '@/models/models'
+import type { IPage, JobListing, PositionFunction } from '@/models/models'
 
 export default class BaseApi {
   public axios!: AxiosStatic
@@ -10,14 +10,32 @@ export default class BaseApi {
     this.axios = axios
   }
 
+  private pageSize: number = 5
+
+  public updatePageSize = (pageSize: string): void => {
+    if (pageSize === 'all') {
+      this.pageSize = 100
+    } else if (pageSize === '25') {
+      this.pageSize = 25
+    } else if (pageSize === '5') {
+      this.pageSize = 5
+    }
+  }
+
   public getPositionFunctions = (): Promise<IPage<PositionFunction>> =>
     this.axios
       .get(`${this.baseUrl}job/position-functions/?page_size=100`)
       .then((response) => response.data)
 
-  /**
-   * TIP: Create a function called getJobListings
-   * Use our public api documentation to find out what endpoint to use
-   * https://test-api.mojob.io/public/docs/
-   */
+  public getJobListings = (): Promise<IPage<JobListing>> =>
+    this.axios
+      .get(
+        `${this.baseUrl}job/listings/?include_open=False&page=1&page_size=${this.pageSize}&use_mojob_feed_filter=True&use_pagination=True`
+      )
+      .then((response) => response.data)
+
+  public getFilteredJobListings = (searchQuery: string): Promise<IPage<JobListing>> =>
+    this.axios
+      .get(`${this.baseUrl}job/position-functions/?search=${searchQuery}`)
+      .then((response) => response.data)
 }
